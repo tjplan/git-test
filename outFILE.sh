c0755eb8579000b3d1e06c2e6ef0c7d98d706b8e
tjplan <omarrod547@gmail.com>
2023-07-19 10:49:12 -0400

    2 file

--- outFILE.sh
+++ outFILE.sh
@@ -1,14 +1,13 @@
 #!/bin/bash 
 #mysqlCommand=$(command -v mysql)
 
+git add -A
+
 #borrar el archivo si existe y si no existe no debe dar error
-if [-f c:/laragon/data/mysql/sample5.txt]
-then
-    rm -rf c:/laragon/data/mysql/sample5.txt
-fi
-mysql -u root -e "SELECT * FROM dbestudiantes.estudiante INTO OUTFILE  'sample5.txt';"
+#rm -rf c:/laragon/data/mysql/sample5.txt
+#mysql -u root -e "SELECT * FROM dbestudiantes.estudiante INTO OUTFILE  'c:/laragon/data/mysql/sample5.txt';"
 #mover el archivo desde la ruta donde esta y copiarlo a C:\git now----
-#mv -f c:/laragon/data/mysql/sample5.txt  c:/git
+mv -f c:/laragon/data/mysql/gitsample5.txt  c:/git-test
 
 
 
